a87e13e75611ed5a7cff64c408eb9dc2f821980f
leskoarpi <71691273+leskoarpi@users.noreply.github.com>
2022-01-20 17:05:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+'''
+5.2 Feladat
+A program, egy eljárás segítségével kirajzol a képernyőre egy 6x3-as mezőt. Alakítsd át ezt a programot úgy, az eljárás hívásakor megadott értékpárnak megfelelően a program az adott pozícióba 'O' helyett '+' jelet írjon ki. A lenti példában az eljárás hivása: mezot_rajzol(0,4)
+
+    O O O O + O
+    O O O O O O
+    O O O O O O
+
+Feladat
+Alakítsd át az előző programot úgy, hogy a felhasználó adhassa meg a koordinátákat, ahová a program 'O' helyett '+' jelet ír. A koordináták bekérése és a mező kirajzolása addig ismétlődjön, amíg a felhasználó negatív számot nem ad meg koordinátaként!
+'''
+
+def mezot_rajzol():
+    szel = int(input('adja meg az x koordinatat (0-tol!): '))
+    hossz =int(input('adja meg az y koordinatat (0-tol!): '))
+    for sor in range(3):
+        for oszlop in range(6):
+            if oszlop == szel and hossz == sor:
+                print('+ ', end='')
+                szel +=10
+            else:
+                print('O ', end='')
+        print()
+        
+mezot_rajzol()
